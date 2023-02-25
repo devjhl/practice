9d9a36faec1600df6eccca6cc3638e67591f6124
jeonghyeon <devhye@naver.com>
2023-02-25 17:00:30 +0900

    환경설정 , 템플릿 설정

---
+++ src/main/java/com/practice/spring/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.practice.spring.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "/main/home";
+	}
+	
+}
